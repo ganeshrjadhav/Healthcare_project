c822174383beb9583dc7a4a459530b436a2dcb16
ganeshrjadhav <ganeshrjadhav@yahoo.com>
2023-05-30 00:03:38 +0530

    'code_delete_column'

--- note_for_part1.py
+++ note_for_part1.py
@@ -31,3 +31,34 @@ display(df)df
 
 ##test code
 
+##Deleted the unnecessary columns
+
+df2 = df.drop('Five Star Data Availability Code')\
+        .drop('Chain Owned')\
+        .drop('EQRS Date')\
+        .drop('STrR Date')\
+        .drop('HGB<10 data availability code')\
+        .drop('Patient Transfusion data availability Code')\
+        .drop('Adult HD Kt/V data availability code')\
+        .drop('Adult PD Kt/V data availability code')\
+        .drop('Percentage of Pediatric HD patients with Kt/V >= 1.2')\
+        .drop('Pediatric HD Kt/V Data Availability Code')\
+        .drop('Number of pediatric HD patient-months with KT/V data')\
+        .drop('Hypercalcemia Data Availability Code')\
+        .drop('Serum phosphorus Data Availability Code')\
+        .drop('Patient Hospitalization data availability Code')\
+        .drop('Patient Hospital Readmission data availability Code')
+
+df2 = df2.drop('Patient Survival data availability code')\
+         .drop('Pediatric PD Kt/V Data Availability Code')\
+         .drop('Number of pediatric PD patient-months with KT/V data')\
+         .drop('Number of pediatric PD patient-months with KT/V data')\
+         .drop('Percentage of pediatric PD patients with Kt/V>=1.8')\
+         .drop('Patient Infection Data Availability Code')\
+         .drop('Fistula data availability code')\
+         .drop('Number of patient-months in nPCR summary')\
+         .drop('nPCR Data Availability Code')\
+         .drop('Percentage of pediatric HD patients with nPCR')\
+         .drop('Patient prevalent transplant waitlist data availability code')
+
+df2.display()
